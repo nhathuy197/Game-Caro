e10e3c842f31e8ec308831d6a217122cc6782da4
nhathuy197 <61338675+nhathuy197@users.noreply.github.com>
2022-06-14 16:31:17 +0700

    game caro

---
+++ GameCaro/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCaro
+{
+    public partial class Main : Form
+    {
+        #region Propertices
+        ChessBoardManager ChessBoard;
+        #endregion
+
+        public Main()
+        {
+            InitializeComponent();
+
+            ChessBoard = new ChessBoardManager(pnlCheckBoard,txbPlayerName, pctbMark);
+      
+            ChessBoard.DrawChessBoard();
+        }
+
+  
+    }
+}
